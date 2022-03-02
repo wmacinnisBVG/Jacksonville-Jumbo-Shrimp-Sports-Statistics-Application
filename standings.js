e2d9420dfa12ff6bcea56dc626a671a998f7b77b
William MacInnis <wmacinnis@bayviewglen.ca>
2022-03-02 13:39:32 -0500

    Table filters work

--- standings.js
+++ standings.js
@@ -1,34 +1,66 @@
 let TEAMS = JSON.parse(localStorage.getItem("teams"));
 const GAMES = JSON.parse(localStorage.getItem("games"));
+const tableHeader = document.getElementById('table-header').innerHTML;
 window.onload = () =>{
     pullStandings('wins');
 }
+const clearTable = () => {
+    document.getElementById('standings-table').innerHTML = "";
+    const tr = document.createElement("tr");
+    tr.innerHTML = tableHeader;
+    document.getElementById('standings-table').append(tr);
+}
+const populateTable = () => {
+    const standingsTable = document.getElementById('standings-table');
+    TEAMS.forEach(team =>{
+        const tr = document.createElement("tr");
+        const teamTd = document.createElement('td');
+        const winsTd = document.createElement('td');
+        const lossesTd = document.createElement('td');
+        const pctTd = document.createElement('td');
+        teamTd.innerHTML = team.team_name;
+        tr.append(teamTd);
+        winsTd.innerHTML = team.wins;
+        tr.append(winsTd);
+        lossesTd.innerHTML = team.losses;
+        tr.append(lossesTd);
+        const pct = team.wins / (team.losses+team.wins);
+        pctTd.innerHTML = pct;
+        tr.append(pctTd);
+        standingsTable.append(tr);
+    });
+}
+const changeTableIcons = (selected) => {
+    let iconIds = ["w-icon", "l-icon", "pct-icon"];
+    let selectedIcon;
+    if(selected === 'wins'){
+        selectedIcon = iconIds[0];
+        delete iconIds[0];
+    } else if (selected === 'losses'){
+        selectedIcon = iconIds[1];
+        delete iconIds[1];
+    } else {
+        selectedIcon = iconIds[2];
+        delete iconIds[2];
+    }
+    iconIds.forEach((icon) => {
+        document.getElementById(icon).className = '';
+        document.getElementById(icon).className = 'fas fa-caret-up';
+    });
+    document.getElementById(selectedIcon).className = '';
+    document.getElementById(selectedIcon).className = 'fas fa-caret-down';
+
+}
 const pullStandings = (filter) =>{
+    clearTable();
     if(filter == 'wins'){
+        changeTableIcons(filter);
         TEAMS = TEAMS.sort((a,b) => (a.wins < b.wins) ? 1 : ((b.wins < a.wins) ? -1 : 0));
-
-        const standingsTable = document.getElementById('tr-container');
-        TEAMS.forEach(team =>{
-            //Add Teams
-            const tr = document.createElement("tr");
-            const teamTd = document.createElement('td');
-            const winsTd = document.createElement('td');
-            const lossesTd = document.createElement('td');
-            const pctTd = document.createElement('td');
-            teamTd.innerHTML = team.team_name;
-            tr.append(teamTd);
-            winsTd.innerHTML = team.wins;
-            tr.append(winsTd);
-            lossesTd.innerHTML = team.losses;
-            tr.append(lossesTd);
-            const pct = team.wins / (team.losses+team.wins);
-            pctTd.innerHTML = pct;
-            tr.append(pctTd);
-            standingsTable.append(tr);
-
-
-
-        });
+        populateTable();
+    } else if(filter == 'losses'){
+        changeTableIcons(filter);
+        TEAMS = TEAMS.sort((a,b) => (a.losses < b.losses) ? 1 : ((b.losses < a.losses) ? -1 : 0));
+        populateTable();
     }
 
 }
